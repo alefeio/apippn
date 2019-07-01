8860118c2be0e0c1f12640b16c1227a482515296
Alexandre Feio <alefeio@gmail.com>
2019-07-01 18:12:40 -0300

    ultima atuaizacao 2

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const server = require('./server')
 
 var port = process.env.PORT || 3000;
 server.listen(port, function () {
-    console.log('Umbler listening on port %s', port);
+    console.log('Listening on port %s', port);
 });
