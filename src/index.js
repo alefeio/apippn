532f4f58477cd529969e1d94f5de7b0f1486ff8c
Alexandre Feio <alefeio@gmail.com>
2019-07-03 17:21:39 -0300

    consertando bug de queda do servidor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const server = require('./server')
 
-var port = process.env.PORT;
+var port = process.env.PORT || 3000;
 server.listen(port, function () {
     console.log('Listening on port %s', port);
 });
