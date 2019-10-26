18645148972eda66993e530151e3c022f424ffe3
Marcelo Daniel <marcelodaniel.daniel@gmail.com>
2019-10-26 12:20:48 -0300

    change port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const server = require('./server')
 
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
 server.listen(port, function () {
     console.log('Listening on port %s', port);
 });
